39a66fa621a145c005299000b3b791b9f70a2562
Rafael Wassoaski <meireles200@hotmail.com>
2021-08-18 20:06:51 -0300

    #time 45m
    * Edição: Adicionando estado aos componentes de nome, sobrenome e cpf, assim como validação no nome e no cpf

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, TextField, Switch, FormControlLabel} from "@material-ui/core";
 
 function SignInForm() {
-
+    const [nome, setNome] = useState('');
+    const [sobreNome, setSobreNome] = useState('');
+    const [cpf, setCpf] = useState('');
 
     return (
-        <form>
-            <TextField id='nome' label='Nome' type='text' variant='outlined' margin='normal' fullWidth/>
-            <TextField id='sobrenome' label='Sobrenome' type='text' variant='outlined' margin='normal' fullWidth/>
-            <TextField id='cpf' label='CPF' type='text' variant='outlined' margin='normal' fullWidth/>
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            console.log(nome);
+            console.log(sobreNome);
+        }}>
+            <TextField
+                value={nome}
+                onChange={(event) => {
+                    let tmpNome = event.target.value;
+                    if (tmpNome.length >= 3) {
+                        tmpNome = tmpNome.substr(0, 3);
+                    }
+                    setNome(tmpNome);
+                }}
+                id='nome' label='Nome' type='text' variant='outlined' margin='normal' fullWidth/>
+
+            <TextField
+                value={sobreNome}
+                onChange={(event) => {
+                    setSobreNome(event.target.value);
+                }}
+                id='sobrenome' label='Sobrenome' type='text' variant='outlined' margin='normal' fullWidth/>
+
+            <TextField
+                value={cpf}
+                onChange={(event) => {
+                    let tmpCPF = event.target.value;
+                    if (tmpCPF.length <= 11) {
+                        setCpf(tmpCPF);
+                    }
+
+                }} id='cpf' label='CPF' type='text' variant='outlined' margin='normal' fullWidth/>
 
             <FormControlLabel control={
                 <Switch name='Promoções' defaultChecked={true} color='primary'/>
