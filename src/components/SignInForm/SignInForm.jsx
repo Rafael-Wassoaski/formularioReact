6a09521206b67f44062a5f8a6e3410d130a6c77f
Rafael Wassoaski <meireles200@hotmail.com>
2021-08-24 20:29:26 -0300

    #time 25m
    * Edição: Adicionando navegação entre formulários. Melhorando legibilidade do código, usando array para controlar a navegação entre os forms

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,17 +1,25 @@
 import React, {Fragment, useState} from "react";
-import {Button, TextField, Switch, FormControlLabel} from "@material-ui/core";
 import PersonalData from "./PersonalData";
 import UserData from "./UserData";
 import DeliveryForm from "./DeliveryForm";
 
 function SignInForm({aoEnviar, validaCPF}) {
+    const [actualStep, setActualStep] = useState(0);
+    const forms = [
+        <UserData aoEnviar={setNextForm}/>,
+        <PersonalData aoEnviar={setNextForm} validaCPF={validaCPF}/>,
+        <DeliveryForm aoEnviar={aoEnviar}/>
+    ];
 
+    function setNextForm() {
+        if (actualStep < 2) {
+            setActualStep(actualStep + 1);
+        }
+    }
 
     return (
         <Fragment>
-            <PersonalData aoEnviar={aoEnviar} validaCPF={validaCPF}/>
-            <UserData/>
-            <DeliveryForm/>
+            {forms[actualStep]}
         </Fragment>
     )
 }
--- src/components/SignInForm/UserData.jsx
+++ src/components/SignInForm/UserData.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {Button, TextField} from "@material-ui/core";
 
-function UserData() {
+function UserData({aoEnviar}) {
     return (
-        <form>
-            <TextField id='email' label='email' type='email' variant='outlined' margin='normal' fullWidth/>
-            <TextField id='senha' label='senha' type='password' variant='outlined' margin='normal' fullWidth/>
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            aoEnviar();
+        }}>
+            <TextField id='email' label='email' type='email' variant='outlined' margin='normal' fullWidth required/>
+            <TextField id='senha' label='senha' type='password' variant='outlined' margin='normal' fullWidth required/>
             <Button type='submit' variant='contained' color='primary'>Cadastrar</Button>
         </form>
     )
